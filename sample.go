56396b62953a10064872ea6c812569e52521dc19
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 13:00:51 +0500

    created sum rec

--- sample.go
+++ sample.go
@@ -9,7 +9,15 @@ func fact(n int) int {
 	return n * fact(n-1)
 }
 
+func sum(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return n + sum(n-1)
+}
+
 func main() {
-	fmt.Println(fact(5))
+	// fmt.Println(fact(5))
+	fmt.Println(sum(5))
 	fmt.Println("Hello World")
 }
