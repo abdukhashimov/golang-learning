1430115899a1f9800d167d44b457c8e621a9df3c
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 17:29:34 +0500

    learned all about channels?no

--- sample.go
+++ sample.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	messages := make(chan string, 2)
+	go func() {
+		messages <- "ping"
+		messages <- "pinasdg"
+		messages <- "pingasd"
+		messages <- "pinasdg"
+		messages <- "asdping"
+		messages <- "ping"
+	}()
+	msg := <-messages
+	msg2 := <-messages
+	msg3 := <-messages
+	fmt.Println(msg)
+	fmt.Println(msg2)
+	fmt.Println(msg3)
 	fmt.Println("Hello World")
 }
