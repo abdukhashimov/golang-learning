efed6b675a0f1e449985d4685b55d8dfa530c51a
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 12:51:00 +0500

    Added sums

--- sample.go
+++ sample.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func sum(nums ...int) {
+	fmt.Println(nums)
+}
+
 func main() {
+	sum(1, 2, 3, 4, 6)
+	nums := []int{1, 2, 3, 4, 5}
+	sum(nums...)
 	fmt.Println("Hello World")
 }
