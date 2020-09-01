2aa64662a3d022cc68533e37393bc187087b1a58
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 12:58:56 +0500

    implemented fact

--- sample.go
+++ sample.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func fact(n int) int {
+	if n == 1 {
+		return n
+	}
+	return n * fact(n-1)
+}
+
 func main() {
+	fmt.Println(fact(5))
 	fmt.Println("Hello World")
 }
