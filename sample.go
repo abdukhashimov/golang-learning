0e487ab054e225371b0e0b5efd74bd7f8c3ab8b5
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 11:36:09 +0500

    removed some lines

--- sample.go
+++ sample.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var str1 = "This is the first string"
-	fmt.Println(str1)
-	for k, char := range str1 {
-		fmt.Printf("%d: %c\n", k, char)
-	}
+	fmt.Println("Hello World")
 }
