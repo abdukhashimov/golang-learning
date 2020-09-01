ee8e4714f5ab2f8bc4c613e8fbb97efb98abcf5b
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 15:56:46 +0500

    learned about methods

--- sample.go
+++ sample.go
@@ -14,8 +14,13 @@ func (r rect) perim() int {
 	return 2 * (r.width + r.height)
 }
 
+func (r rect) allSum() int {
+	return r.width + r.height
+}
+
 func main() {
 	r := rect{height: 2, width: 4}
+	fmt.Println(r.allSum())
 	fmt.Println("area: ", r.area())
 	fmt.Println("perimetr: ", r.perim())
 	rp := &r
