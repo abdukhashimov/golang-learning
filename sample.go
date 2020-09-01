f29710703220a13842878408d76c0356c62a2023
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 12:30:44 +0500

    repeated the for loop five times

--- sample.go
+++ sample.go
@@ -3,5 +3,35 @@ package main
 import "fmt"
 
 func main() {
+	// make a for loop to iterate over 3 times
+	i := 1
+	for i < 3 {
+		fmt.Println("Hello World")
+		i++
+	}
+	j := 1
+	for j < 3 {
+		fmt.Println("Hello World")
+		j++
+	}
+	j1 := 2
+	for j1 < 3 {
+		fmt.Println("Hello world")
+	}
+	j3 := 1
+	for j3 < 3 {
+		fmt.Println("Hello Worldi")
+	}
+	j4 := 1
+	for j4 < 3 {
+		fmt.Println("Hello world")
+	}
+	j5 := 1
+	for j5 < 5 {
+		fmt.Println("Hello world")
+	}
+	if j5 == 1 {
+		fmt.Println("Hello world")
+	}
 	fmt.Println("Hello World")
 }
