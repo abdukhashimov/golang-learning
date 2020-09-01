dbec2279d3d7ae7bd0e653ed1f44772c469941ae
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 13:07:00 +0500

    started with creating an admin struct
    implemented structs

--- sample.go
+++ sample.go
@@ -2,6 +2,49 @@ package main
 
 import "fmt"
 
+type Name struct {
+	name    string
+	age     int
+	isAdmin bool
+}
+
+type User struct {
+	name     string
+	age      int
+	isAdmin  bool
+	lastname string
+}
+
+type User2 struct {
+	firstname string
+	lastname  string
+	age       int
+	isAdmin   bool
+	paidTill  string
+}
+
+type User3 struct {
+	firstname string
+	lastname  string
+	age       int
+	isAdmin   bool
+	paidTill  string
+}
+
+type User4 struct {
+	firstname string
+	lastname  string
+	age       int
+	isAdmin   bool
+	paidTill  string
+}
+
+type Name struct {
+	field  int
+	field2 string
+	field3 bool
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
