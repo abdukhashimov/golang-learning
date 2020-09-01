f8a71e68955f0fffa09ba514f81b904eedc48237
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 11:21:06 +0500

    string is worked
    iterated over strings

--- sample.go
+++ sample.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var str1 = "This is the first string"
+	fmt.Println(str1)
+	for k, char := range str1 {
+		fmt.Printf("%d: %c\n", k, char)
+	}
 }
