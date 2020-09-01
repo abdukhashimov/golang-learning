6dc4bcfcbcad17c8103ce08954b53f1a496e6c9a
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 16:51:41 +0500

    ASdasd

--- sample.go
+++ sample.go
@@ -1,6 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func f1(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("Can't work with 42")
+	}
+	return arg + 3, nil
+}
+
+type argError struct {
+	arg  int
+	prob string
+}
+
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "Cant work with it"}
+	}
+	return arg + 3, nil
+}
 
 func main() {
 	for _, i := range []int{7, 42} {
