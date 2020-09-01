b94b629d79c5578f75757e5041fae855f02ed4ba
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 12:57:22 +0500

    defined an function

--- sample.go
+++ sample.go
@@ -7,8 +7,12 @@ func sum(nums ...int) {
 }
 
 func main() {
-	sum(1, 2, 3, 4, 6)
-	nums := []int{1, 2, 3, 4, 5}
-	sum(nums...)
+	// sum(1, 2, 3, 4, 6)
+	// nums := []int{1, 2, 3, 4, 5}
+	// sum(nums...)
+	myOutput := func() int {
+		return 1
+	}
+	fmt.Println(myOutput())
 	fmt.Println("Hello World")
 }
